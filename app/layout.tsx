8aeede02482417803f88295c1ada54c39a1b04d3
Mateo <tu@email.com>
2026-01-17 22:04:46 +0100

    Actualizado favicon con nueva imagen

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export const metadata: Metadata = {
     "transformación digital"
   ],
   icons: {
-    icon: '/favicon.png?v=3',
+    icon: '/favicon.png?v=4',
   },
   robots: {
     index: true,
